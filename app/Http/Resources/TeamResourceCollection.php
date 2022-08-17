1ba04cdcdeb35f01c671daadd14e0003c730dfae
linsas <110762866+linsas@users.noreply.github.com>
2022-08-17 19:55:13 +0300

    Team API

---
+++ app/Http/Resources/TeamResourceCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class TeamResourceCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return $this->collection->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'name' => $item->name,
+            ];
+        });
+    }
+}
---
+++ app/Services/TeamService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use App\Models\Team;
+
+class TeamService
+{
+    public function index()
+    {
+        return Team::all();
+    }
+
+    public function store($inputData)
+    {
+        $validator = Validator::make($inputData, [
+            'name' => 'required|string|max:255',
+        ]);
+        $validData = $validator->validate();
+
+        $entry = new Team;
+        $entry->name = $validData['name'];
+        $entry->save();
+    }
+
+    public function findOrFail($id)
+    {
+        if (!ctype_digit($id)) {
+            throw ValidationException::withMessages(['The id is invalid.']);
+        }
+        $entry = Team::findOrFail($id);
+        return $entry;
+    }
+
+    public function update($inputData, $id)
+    {
+        $entry = $this->findOrFail($id);
+
+        $validator = Validator::make($inputData, [
+            'name' => 'required|string|max:255',
+        ]);
+        $validData = $validator->validate();
+
+        $entry->name = $validData['name'];
+        $entry->save();
+    }
+
+    public function destroy($id)
+    {
+        $entry = $this->findOrFail($id);
+        $entry->delete();
+    }
+}
