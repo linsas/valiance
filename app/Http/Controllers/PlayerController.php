5c29786427b0b70d103acb522cd06bcc106da54b
linsas <110762866+linsas@users.noreply.github.com>
2022-08-18 19:13:08 +0300

    Player API

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\PlayerService;
+use App\Http\Resources\PlayerResource;
+use App\Http\Resources\PlayerResourceCollection;
+
+class PlayerController extends Controller
+{
+    protected $service;
+
+    public function __construct(PlayerService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index()
+    {
+        $list = $this->service->index();
+        return new PlayerResourceCollection($list);
+    }
+
+    public function store(Request $request)
+    {
+        $inputData = $request->json()->all();
+        $this->service->store($inputData);
+        return response()->noContent();
+    }
+
+    public function show($id)
+    {
+        $entry = $this->service->findOrFail($id);
+        return new PlayerResource($entry);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputData = $request->json()->all();
+        $this->service->update($inputData, $id);
+        return response()->noContent();
+    }
+
+    public function destroy($id)
+    {
+        $this->service->destroy($id);
+        return response()->noContent();
+    }
+}
---
+++ app/Http/Resources/PlayerResourceCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PlayerResourceCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return $this->collection->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'alias' => $item->alias,
+                'team' => $item->team == null ? null : [
+                    'id' => $item->fk_team,
+                    'name' => $item->team->name,
+                ],
+            ];
+        });
+    }
+}
