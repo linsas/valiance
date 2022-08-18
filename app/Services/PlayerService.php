9f076e10a6e5d6c812c97ee884984d10c6679d0c
linsas <linsas0107@gmail.com>
2022-08-18 19:13:08 +0300

    Player API

---
+++ app/Services/PlayerService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use App\Models\Player;
+
+class PlayerService
+{
+    public function index()
+    {
+        return Player::all();
+    }
+
+    public function store($inputData)
+    {
+        $validator = Validator::make($inputData, [
+            'alias' => 'required|string|max:255',
+            'team' => 'nullable|integer|exists:team,id',
+        ]);
+        $validData = $validator->validate();
+
+        $entry = new Player;
+        $entry->alias = $validData['alias'];
+        $entry->fk_team = $validData['team'] ?? null;
+        $entry->save();
+    }
+
+    public function findOrFail($id)
+    {
+        if (!ctype_digit($id)) {
+            throw ValidationException::withMessages(['The id is invalid.']);
+        }
+        $entry = Player::findOrFail($id);
+        return $entry;
+    }
+
+    public function update($inputData, $id)
+    {
+        $entry = $this->findOrFail($id);
+
+        $validator = Validator::make($inputData, [
+            'alias' => 'required|string|max:255',
+            'team' => 'nullable|integer|exists:team,id',
+        ]);
+        $validData = $validator->validate();
+
+        $entry->alias = $validData['alias'];
+        $entry->fk_team = $validData['team']; // ?? null;
+        $entry->save();
+    }
+
+    public function destroy($id)
+    {
+        $entry = $this->findOrFail($id);
+        $entry->delete();
+    }
+}
