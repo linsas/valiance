90ad0bd72fd84cd5502bd87d0cd8ec3683dfc9a2
linsas <linsas0107@gmail.com>
2022-08-18 20:22:17 +0300

    Added player controls

---
+++ resources/js/pages/Player/PlayerCreate.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Fab } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+
+import useFetch from '../../utility/useFetch'
+import PlayerForm from './PlayerForm'
+
+function PlayerCreate({ update }) {
+	const [formOpen, setFormOpen] = React.useState(false)
+	const [isCreating, fetchCreate] = useFetch('/api/players', 'POST')
+
+	const onSubmit = (player) => {
+		fetchCreate({ alias: player.alias, team: player.team?.id ?? null }).then(() => update(), console.error)
+	}
+
+	return <>
+		<Fab color='primary' onClick={() => setFormOpen(true)} style={{ position: 'fixed', bottom: 16, right: 16 }}>
+			<AddIcon />
+		</Fab>
+		<PlayerForm open={formOpen} player={{ alias: '', team: null }} onSubmit={onSubmit} onClose={() => setFormOpen(false)} />
+	</>
+}
+
+export default PlayerCreate
