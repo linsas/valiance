6b247b52e2ecde79647fb079440ca2fec16d859e
linsas <linsas0107@gmail.com>
2022-08-20 13:10:02 +0300

    Added participations to seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,6 +20,26 @@ class DatabaseSeeder extends Seeder
             if (rand(1, 5) <= 4) $player->update([ 'fk_team' => Team::inRandomOrder()->first()->id ]);
         });
 
-        factory(Tournament::class, 10)->create();
+        factory(Tournament::class, 15)->create()->each(function ($tournament) {
+            $allTeams = \App\Models\Team::inRandomOrder()->get();
+            for ($i = 0; $i < 8; $i++) { // fills 8 teams per tournament
+                $team = $allTeams[$i];
+                $participant = \App\Models\TournamentTeam::create([
+                    'fk_tournament' => $tournament->id,
+                    'fk_team' => $team->id,
+                    'name' => $team->name,
+                    'seed' => $i + 1,
+                ]);
+                for ($j = 0; $j < $team->players->count(); $j++) {
+                    $player = $team->players[$j];
+                    \App\Models\TournamentTeamPlayer::create([
+                        'fk_player' => $player->id,
+                        'fk_tournament_team' => $participant->id,
+                    ]);
+                }
+            }
+        });
+
+        factory(Tournament::class, 5)->create();
     }
 }
