bdb9fd8804a91d63d5dff505ef0e3327b6a1cf30
linsas <110762866+linsas@users.noreply.github.com>
2023-05-18 19:42:19 +0300

    Add advanced footer

--- resources/js/main/Footer.tsx
+++ resources/js/main/Footer.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import { Box, Typography } from '@mui/material'
 
 function Footer() {
+	const envObject = process.env
+
+	const [visible, setVisible] = React.useState(false)
+
+	if (envObject == null) return null
+
 	return <>
 		<Box sx={{ marginTop: 2, padding: 2, opacity: 0.2 }} component='footer'>
-			<Typography color='textSecondary' variant='body2'>&copy; 2022</Typography>
+			<Typography
+				color='textSecondary'
+				variant='body2'
+				component='span'
+				onMouseEnter={() => setVisible(true)}
+				onMouseLeave={() => setVisible(false)}
+				sx={{ opacity: visible ? 1 : 0 }}
+			>
+				{envObject.revision} - {envObject.buildDate}
+			</Typography>
 		</Box>
 	</>
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const webpack = require('webpack');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,9 +12,24 @@ const mix = require('laravel-mix');
  |
  */
 
+let revision = require('child_process')
+    .execSync('git rev-parse --short HEAD')
+    .toString()
+    .trim()
+
 mix
     .ts('resources/js/app.tsx', 'public/js')
     .react()
     .sourceMaps()
     .disableNotifications()
     .browserSync('127.0.0.1:8000')
+    .webpackConfig({
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': JSON.stringify({
+                    revision,
+                    buildDate: new Date().toISOString()
+                })
+            })
+        ]
+    })
